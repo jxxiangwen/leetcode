16b1e0db26ec8cfa07573154f9f8e92afc9350a2
邹祥文 <xiangwen.zou@ymm56.com>
2020-05-15 15:50:01 +0800

    415.addStrings done

---
+++ src/main/java/com/xiang/wen/easy/AddStrings_415.java
@@ -0,0 +1,45 @@
+package com.xiang.wen.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author jxxiangwen
+ * Mail: xiangwen.zou@ymm56.com
+ * Time: 20-5-15 下午2:54
+ */
+public class AddStrings_415 {
+
+    public String addStrings(String num1, String num2) {
+        if (null == num1 || null == num2) {
+            return "";
+        }
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        char[] result = new char[Math.max(num1.length(), num2.length()) + 1];
+        int length = result.length - 1;
+        while (i >= 0 || j >= 0) {
+            int n1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int n2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int res = n1 + n2 + carry;
+            result[length] = (char) ((res % 10) + '0');
+            carry = res / 10;
+            i--;
+            j--;
+            length--;
+        }
+        if (carry == 1) {
+            result[length] = '1';
+        }
+        return new String(result).trim();
+    }
+
+    @Test
+    public void addStrings() {
+        Assert.assertEquals(addStrings("125", "456"), "581");
+        Assert.assertEquals(addStrings("1", "9"), "10");
+    }
+
+
+}
